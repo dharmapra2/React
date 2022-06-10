8163d2e90fb5e0c23dc9f22922df29b430172e33
dharmapra2 <dharmapra865@gmail.com>
2022-06-10 23:55:02 +0530

    coomit

---
+++ todo/src/Components/List.jsx
@@ -0,0 +1,56 @@
+import { useEffect } from "react";
+export default function List({ todoLists, setTodoLists, setEdit }) {
+  // console.log(todoLists);
+  
+  //to load in localStorage
+  // useEffect(() => {
+  //   let arr_item = window.localStorage.getItem('Todo')?window.localStorage.getItem('Todo'):[];
+  //   console.log(arr_item);
+  // }, [todoLists]);
+
+  function editBtn(item) {
+    let h = todoLists.filter((data) => data.id === item);
+    // console.log(h[0]);
+    setEdit(h[0]);
+  }
+  function deleteList(item) {
+    // console.log(item);
+    setTodoLists(todoLists.filter((data, index) => index !== item));
+    // console.log(todoLists);
+  }
+  return (
+    <div className="task_lists">
+      {todoLists.map((item, index) => {
+        return (
+          <div
+            className="input-group justify-content-center d-flex input-group-sm"
+            key={index}
+          >
+            <input
+              type="text"
+              name="text"
+              id=""
+              value={item.text}
+              className="form-control form-control-sm m-1 "
+              readOnly
+              disabled
+            />
+            <button
+              className="btn btn-outline-warning m-1"
+              onClick={() => editBtn(item.id)}
+            >
+              Edit
+            </button>
+
+            <button
+              className="btn btn-outline-danger m-1"
+              onClick={() => deleteList(index)}
+            >
+              Delete
+            </button>
+          </div>
+        );
+      })}
+    </div>
+  );
+}
